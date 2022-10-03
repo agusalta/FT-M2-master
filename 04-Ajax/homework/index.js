3081a3ea1ba34e546fa40391fc50352d0a2017f8
agus <agusalta002@gmail.com>
2022-10-03 09:47:48 -0300

    05

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -2,6 +2,8 @@
 // ver amigos
 $('#boton').click(() => {
     var lista = $('#lista');
+    lista.empty();
+
     $.get('http://localhost:5000/amigos', function(data) {
        data.forEach(el => lista.append(`<li>${el.name}</li>`))
     })
@@ -15,3 +17,14 @@ $('#search').click(() => {
     })
 })
 
+// borrar amigos
+$('#delete').click(() => {
+    var id = $('inputDelete').val();
+    $.ajax({
+        url: `http://localhost:5000/amigos/${id}`, // === "palabra" + "var"
+        type: 'DELETE',
+        success: () => {
+            $('#success').text(`tu amigo numero ${id} fue borrado de la lista`)
+        }
+    })
+})
